6245ab76e3f8d6b10233453884e8ba301b6c1fae
Henry Bergström <henrybergstrom@protonmail.com>
2025-09-01 13:41:11 +0200

    navlinks done

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,20 @@
+import { Link, NavLink } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import "../App.css";
 
 
+// Uppgift: Lägg till en navbar till Layout
 function Layout(props) {
     return ( <>
         <Header/>
+        <nav>
+            <ul>
+                <li> <NavLink to="/" className={({isActive}) => isActive ? "active-link" : "link"}>Home</NavLink> </li>
+                <li> <NavLink to="/products" className={({isActive}) => isActive ? "active-link" : "link"}>Products</NavLink> </li>
+                <li> <NavLink to="/profile" className={({isActive}) => isActive ? "active-link" : "link"}>Profile</NavLink> </li>
+            </ul>
+        </nav>
         { /* Huvudinnehåll ska ligga here */ }
         {props.children}
         <Footer/>
